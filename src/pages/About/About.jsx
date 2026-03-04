3a8e1ed50bbbba2d356b8e3d2fe7facf1463d51e
Atila Ferreira de Bastos <atila.bastos@victumservices.com.br>
2026-03-04 18:22:13 -0300

    criação do ambiente sobre

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,3 +1,20 @@
-export default function About() {
-    return <h1>About</h1>;
+import styles from "./About.module.css";
+import { Link } from "react-router-dom";
+
+const About = () => {
+    return (
+        <div className={styles.About}>
+            <h2>Sobre o TI <span>BLOG</span></h2>
+            <p>
+                Este Projeto consiste em um blog feito com REACT no front-end e Firebase no back-end, foi um projeto que desenvolvi para obter conhecimento em desenvolvimento.
+            </p>
+            <Link to="/post/create" className="btn">
+                Criar post
+            </Link>
+
+        </div>
+    )
 }
+
+export default About
+
